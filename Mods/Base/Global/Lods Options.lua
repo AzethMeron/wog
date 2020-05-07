de9c3c641d5d6d55d1a85568bf12604afdf04b7b
Jakub <41695668+AzethMeron@users.noreply.github.com>
2020-05-08 00:22:19 +0200

    Rearanched options, OldWog trees

--- Mods/Base/Global/Lods Options.lua
+++ Mods/Base/Global/Lods Options.lua
@@ -5,6 +5,8 @@ function global.events.EnterMap()
 		LD:T(?v, 11, ModName.."/Lods/Trees.lod")
 	elseif Options.Trees == "move" then
 		LD:T(?v, 11, ModName.."/Lods/TreeMove.lod")
+	elseif Options.Trees == "oldwog" then
+		LD:T(?v, 11, ModName.."/Lods/OldWog.lod")
 	end
 	if Options.NewGrid then
 		LD:T(?v, 11, ModName.."/Lods/Grid.lod")
