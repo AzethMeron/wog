e9f38d174528755ae97b86aac9acc71b2bb2a4bb
Jakub <41695668+AzethMeron@users.noreply.github.com>
2020-06-18 10:26:02 +0200

    wip

--- Mods/NewDawn/Map/cosmetic_music.lua
+++ Mods/NewDawn/Map/cosmetic_music.lua
@@ -8,3 +8,38 @@ local mp3_dir = ModName.."\\Res\\mp3\\"
 local mp3_relative_dir = "..\\Mods\\"
 local GetRandFile = Lib.GetRandFileMods
 
+SetMusic = function(index, mod_relative_path)
+  if(mod_relative_path == nil) then Lib.PrintError("SetMusic","Settiing nil music at index: "..index.." Not actual error, more warning") return; end
+  MP:S(index,mp3_relative_dir..mod_relative_path)
+end
+
+ShuffleMusicAi = function()
+  
+end
+
+ShuffleMusicCombat = function()
+  
+end
+
+ShuffleMusicTown = function()
+  local masks_town = { 
+    [1] = "cstletown*.mp3",
+    [2] = "rampart*.mp3",
+    [3] = "towertown*.mp3",
+    [4] = "infernotown*.mp3",
+    [5] = "necrotown*.mp3",
+    [6] = "dungeon*.mp3",
+    [7] = "stronghold*.mp3",
+    [8] = "fortresstown*.mp3",
+    [9] = "elemtown*.mpe"
+    }
+end
+
+ShuffleMusicTerrain = function()
+  
+end
+
+ShuffleMusicBattleresults = function()
+  
+end
+
