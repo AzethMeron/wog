42a1d69479e975ccef0c9d116343a761473c948a
AzethMeron <meronentertainment@gmail.com>
2020-06-25 17:57:01 +0200

    Swapping PC. Final commit on this one

--- Mods/newdawn/Map/cosmetic_buy_cr.lua
+++ Mods/newdawn/Map/cosmetic_buy_cr.lua
@@ -3,3 +3,21 @@
 local Lib = require "Lib"
 local LibStr = require "LibStr"
 
+CM(1).? = function()
+	local click_type = CM:S(?v)
+	if(click_type ~= 12) then return; end
+	local ob_number = CM:I(?v)
+	if(ob_number ~= 158) then return end -- !!! for tests, should be replaced with 159
+	-- rightclick on castle icon
+	local cr_basic, cr_upgraded = {} {}
+	
+	for i = 0,6,1 do
+		cr_basic[i], cr_upgraded[i] = CA(-1):M1(i,?v,?v)
+	end
+	
+	local res_gold = OW:R(-1,6,?v)
+	local res_mercury = OW:R(-1,1,?v)
+	local res_sulfur = OW:R(-1.3.?v)
+	local res_crystal = OW:R(-1,4,?v)
+	local res_gems = OW:R(-1,5,?v) 
+end
