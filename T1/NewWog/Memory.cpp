293625ae4d2c88478d4cfed879cc13c2a4099131
Jakub <meronentertainment@gmail.com>
2020-10-24 18:56:52 +0200

    work in progress

--- T1/NewWog/Memory.cpp
+++ T1/NewWog/Memory.cpp
@@ -11,6 +11,8 @@
 #define __FILENUM__ 34
 #include "List.h"
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 struct allocated_memory {
 	Byte* memory; // address in memory
 	unsigned int length; // length in bytes
@@ -23,3 +25,35 @@ struct allocated_memory {
 
 List<allocated_memory> mem;
 
+int mem_save();
+int mem_load();
+int mem_add(const char* id, const unsigned int length);
+allocated_memory* mem_access(const char* id);
+void mem_clean();
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// cool but remember it works ONLY with static address
+struct patch {
+	Byte* to; // address where the data will be copied
+	Byte* from; // address from which data will be copied
+	Byte* prev_val; // backup of data, to reverse patching later
+	unsigned int length; // length of data in bytes
+
+	patch() { 
+		to = NULL; 
+		from = NULL; 
+		prev_val = NULL; 
+		length = 0; 
+	}
+};
+
+List<patch> patches;
+
+void patch_apply(patch*);
+void patch_reverse(patch*);
+
+int patch_save();
+int patch_load();
+int patch_add(Byte* to, Byte* from, unsigned int length);
+void patch_clean();
